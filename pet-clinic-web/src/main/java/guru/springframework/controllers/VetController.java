9ab615f674c251e5ec6aeb2dcbf2d5406523b98d
serghalak <serghalak@gmail.com>
2020-04-17 20:04:00 +0300

    adding vets page and controller. Closes #7

---
+++ pet-clinic-web/src/main/java/guru/springframework/controllers/VetController.java
@@ -0,0 +1,34 @@
+package guru.springframework.controllers;
+
+
+import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.services.VetService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
+
+@Controller
+public class VetController {
+
+    private VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/vets","/vets/index","/vets/index.html","/vets.html"})
+    public String listVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
+        return "vets/index";
+    }
+
+    @RequestMapping("/api/vets")
+    public @ResponseBody
+    Set<Vet> getVetsJson(){
+        Set<Vet> vetServiceAll = vetService.findAll();
+        return vetServiceAll;
+    }
+}
