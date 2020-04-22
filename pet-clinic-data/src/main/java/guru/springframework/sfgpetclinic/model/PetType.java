46fd9bdb6d0baaf8a7ae2738f9902ab9d57361ce
serghalak <serghalak@gmail.com>
2020-04-22 19:13:08 +0300

    adding spring Config lesson75 Closes #14

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -1,14 +1,41 @@
 package guru.springframework.sfgpetclinic.model;
 
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.Set;
+
+@Setter
+@Getter
+@NoArgsConstructor
+//@AllArgsConstructor
+//@Builder
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity{
+
+    @Builder
+    public PetType(Long id, String name) {
+        super(id);
+
+        this.name=name;
+    }
+
 
     private String name;
 
-    public String getName() {
-        return name;
-    }
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "petType")
+    private Set<Pet> pet;
 
-    public void setName(String name) {
-        this.name = name;
+    @Override
+    public String toString() {
+        return name;
     }
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,4 +1,23 @@
 package guru.springframework.sfgpetclinic.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable( name = "vet_specialty"
+            ,joinColumns =@JoinColumn(name="vet_id",referencedColumnName = "id")
+            ,inverseJoinColumns = @JoinColumn(name = "specialty_id",referencedColumnName = "id"))
+    private Set<Specialty> specialities =new HashSet<>();
 }
